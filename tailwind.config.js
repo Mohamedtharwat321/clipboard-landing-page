7cbfbf95936be174b4960905d0276f291061b9f5
Mohamed Tharwat <mohamedsharkway201@gmail.com>
2023-11-11 14:35:37 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export const content = ["./**/*.{html,js}"];
+export const theme = {
+  extend: {
+    colors:{
+  StrongCyan: 'hsl(171, 66%, 44%)',
+  LightBlue: 'hsl(233, 100%, 69%)',
+  DarkBlue: 'hsl(210, 10%, 33%)',
+  GrayishBlue: 'hsl(201, 11%, 66%)',
+  },
+  
+  backgroundImage: {
+  'hero-img': "url('/images/bg-header-desktop.png')",
+  },
+  },
+};
+export const plugins = [];
+
+
